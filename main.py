1a56e41cc9666c84fcc878b517880ced9a5ac7c8
Aditya Dahake <dahakeadi15@gmail.com>
2025-03-12 15:37:34 +0530

    (feat) setup user input

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import ui
 def main():
     user_input = "start"
     is_game_over = False
+    feedback_to = ""
     while not is_game_over:
         ui.clear_screen()
         print(ui.GAME_TITLE)
@@ -19,9 +20,19 @@ def main():
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
         ]
         ui.display_grid(game_state, len(game_state))
-        user_input = input("End game? (y/n)\n>>> ")
-        if user_input == "y":
-            is_game_over = True
+        # TODO: Set up game states instead of this and change game_state to player_state
+        print(feedback_to)
+        feedback_to = ""
+        print(ui.GAME_OPTIONS)
+
+        user_input = input(">>> ")
+        match user_input:
+            case "n":
+                feedback_to = ui.OF_NEW_GAME
+            case "e":
+                is_game_over = True
+            case _:
+                feedback_to = ui.OF_INVALID_OPTION
 
 
 if __name__ == "__main__":
