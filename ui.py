502be6b18e4a3a48f2f3297fd0a991261aeaff49
Aditya Dahake <dahakeadi15@gmail.com>
2025-03-12 14:55:11 +0530

    (feat) add colors to the ui

--- ui.py
+++ ui.py
@@ -1,5 +1,7 @@
 import os
 
+from colorize import TextColor, color_text
+
 GAME_TITLE = """
  __ _ ____ _  _ ____ _      ____ ____ _  _ ___  ____ ___
  | \| |--|  \/  |--| |___   |___ [__] |\/| |==] |--|  | 
@@ -7,24 +9,25 @@ GAME_TITLE = """
 """
 
 # GAME OBJECTS
-SEA = "  ~  "
-SHIP = " <=> "
-WRECKAGE = " <x> "
+SEA = color_text("  ~  ", TextColor.BRIGHT_BLUE)
+SHIP = color_text(" <=> ", TextColor.BRIGHT_GREEN)
+WRECKAGE = color_text(" <x> ", TextColor.BRIGHT_RED)
 
 
+# GAME GRID
 def display_grid(game_state: list[list[int]], grid_size):
     c = 65
 
     # TODO: Fix grid for double digit grid size
 
-    print("     |", end="")
+    print(color_text("     |", TextColor.GRAY), end="")
     for i in range(grid_size):
-        print(f"  {i+1}  ", end="")
-    print("|")
-    print((grid_size * 5 + 7) * "-")
+        print(color_text(f"  {i+1}  ", TextColor.GRAY), end="")
+    print(color_text("|", TextColor.GRAY))
+    print(color_text((grid_size * 5 + 7) * "-", TextColor.GRAY))
 
     for i in range(grid_size):
-        print(f"  {chr(c+i)}  |", end="")
+        print(color_text(f"  {chr(c+i)}  |", TextColor.GRAY), end="")
         for j in range(grid_size):
             match game_state[i][j]:
                 case 0:
@@ -35,9 +38,9 @@ def display_grid(game_state: list[list[int]], grid_size):
                     print(WRECKAGE, end="")
                 case _:
                     print("  .  ", end="")
-        print("|")
+        print(color_text("|", TextColor.GRAY))
 
-    print((grid_size * 5 + 7) * "-")
+    print(color_text((grid_size * 5 + 7) * "-", TextColor.GRAY))
 
 
 def clear_screen():
